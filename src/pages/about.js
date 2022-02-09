3d95a0db7b568c478d87661c3f2783c37d1559b2
xiouchin32 <xiouchin32@gmail.com>
2022-02-09 16:38:10 +0800

    add link test

---
+++ src/pages/about.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Link } from "gatsby";
+
+const about = () => {
+    return (
+        <div>
+            <h1>Hi! About Page!</h1>
+            <Link to="/">Go Home</Link>
+        </div>
+    );
+};
+
+export default about;
